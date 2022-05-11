2fdf79d8039dff8786392a63651ad4c373289aeb
Mykhailo <mp4clips@gmail.com>
2022-05-11 11:06:06 +0300

    adds form inputs and an empty submit handler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 class ProjectInput {
   private templateEl: HTMLTemplateElement;
   private hostEl: HTMLDivElement;
-  private templateContent: HTMLFormElement;
+  private formEl: HTMLFormElement;
+
+  private inputTitleEl: HTMLInputElement;
+  private inputDescriptionEl: HTMLTextAreaElement;
+  private inputPeopleEl: HTMLInputElement;
 
   constructor() {
     this.templateEl = document.getElementById('project-input') as HTMLTemplateElement;
@@ -9,13 +13,32 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateEl.content, true);
 
-    this.templateContent = importedNode.firstElementChild as HTMLFormElement;
+    this.formEl = importedNode.firstElementChild as HTMLFormElement;
+    this.formEl.id = 'user-input';
 
+    this.inputTitleEl = document.querySelector('#title') as HTMLInputElement;
+    this.inputDescriptionEl = document.querySelector('#description') as HTMLTextAreaElement;
+    this.inputPeopleEl = document.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
+  private handleSubmit(event: Event) {
+    event.preventDefault();
+
+    console.log('ADD PROJECT clicked', this.formEl);
+    console.log('titleEl', this.inputTitleEl);
+    console.log('descriptionEl', this.inputDescriptionEl);
+    console.log('peopleEl', this.inputPeopleEl);
+  }
+
+  private configure() {
+    this.formEl.addEventListener('submit', this.handleSubmit.bind(this));
+  }
+
   private attach() {
-    this.hostEl.insertAdjacentElement("afterbegin", this.templateContent);
+    this.hostEl.insertAdjacentElement("afterbegin", this.formEl);
   }
 }
 
