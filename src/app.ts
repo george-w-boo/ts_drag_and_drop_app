7a1c108851cae370bfad9f2148e1539087682e0d
Mykhailo <mp4clips@gmail.com>
2022-05-11 15:12:40 +0300

    adds fetching user input and basic validation

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,8 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
       const adjMethod = originalMethod.bind(this);
 
       return adjMethod;
-    }
-  }
+    },
+  };
 
   return adjDescriptor;
 }
@@ -25,34 +25,70 @@ class ProjectInput {
   private inputPeopleEl: HTMLInputElement;
 
   constructor() {
-    this.templateEl = document.getElementById('project-input') as HTMLTemplateElement;
-    this.hostEl = document.getElementById('app') as HTMLDivElement;
+    this.templateEl = document.getElementById(
+      "project-input"
+    ) as HTMLTemplateElement;
+
+    this.hostEl = document.getElementById("app") as HTMLDivElement;
 
     const importedNode = document.importNode(this.templateEl.content, true);
 
     this.formEl = importedNode.firstElementChild as HTMLFormElement;
-    this.formEl.id = 'user-input';
+    this.formEl.id = "user-input";
 
-    this.inputTitleEl = document.querySelector('#title') as HTMLInputElement;
-    this.inputDescriptionEl = document.querySelector('#description') as HTMLTextAreaElement;
-    this.inputPeopleEl = document.querySelector('#people') as HTMLInputElement;
+    this.inputTitleEl = this.formEl.querySelector('#title')! as HTMLInputElement;
 
-    this.configure();
+    this.inputDescriptionEl = this.formEl.querySelector(
+      "#description"
+    ) as HTMLTextAreaElement;
+    this.inputPeopleEl = this.formEl.querySelector("#people") as HTMLInputElement;
+
     this.attach();
+    this.configure();
+
+    console.log('this.inputTitleEl', document.getElementById('title'));
+  }
+
+  private clearInputs() {
+    this.inputTitleEl.value = "";
+    this.inputDescriptionEl.value = "";
+    this.inputPeopleEl.value = "";
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    console.log('ssdfs');
+    const title = this.inputTitleEl.value;
+    const description = this.inputDescriptionEl.value;
+    const peopleAmount = this.inputPeopleEl.value;
+
+    if (
+      title.trim().length === 0 ||
+      description.trim().length === 0 ||
+      peopleAmount.trim().length === 0
+    ) {
+      alert(
+        "An invalit input filed. Please, make sure all the fields are not empty"
+      );
+      return;
+    }
+
+    return [title, description, +peopleAmount];
   }
 
   @autobind
   private handleSubmit(event: Event) {
     event.preventDefault();
+    console.log('submitted');
 
-    console.log('ADD PROJECT clicked', this.formEl);
-    console.log('titleEl', this.inputTitleEl);
-    console.log('descriptionEl', this.inputDescriptionEl);
-    console.log('peopleEl', this.inputPeopleEl);
+    const userInput = this.gatherUserInput();
+
+    console.log("userInput", userInput);
+
+    this.clearInputs();
   }
 
   private configure() {
-    this.formEl.addEventListener('submit', this.handleSubmit);
+    this.formEl.addEventListener("submit", this.handleSubmit);
   }
 
   private attach() {
