72538ef9d0a3386f5295a683b03526a2622aa361
Mykhailo <mp4clips@gmail.com>
2022-05-11 16:24:37 +0300

    adds more advanced validation

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,53 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
       const adjMethod = originalMethod.bind(this);
 
       return adjMethod;
-    }
-  }
+    },
+  };
 
   return adjDescriptor;
 }
-// end of autobind decorator
+// validation
+interface Validatable {
+  value: number | string;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(objToValidate: Validatable): boolean {
+  let isValid = true;
+
+  if (objToValidate.required) {
+    isValid = isValid && objToValidate.value.toString().length > 0;
+  }
+
+  if (
+    objToValidate.minLength != null &&
+    typeof objToValidate.value === "string"
+  ) {
+    isValid = isValid && objToValidate.value.length >= objToValidate.minLength;
+  }
+
+  if (
+    objToValidate.maxLength != null &&
+    typeof objToValidate.value === "string"
+  ) {
+    isValid = isValid && objToValidate.value.length <= objToValidate.maxLength;
+  }
+
+  if (objToValidate.min != null && typeof objToValidate.value === "number") {
+    isValid = isValid && objToValidate.value >= objToValidate.min;
+  }
+
+  if (objToValidate.max != null && typeof objToValidate.value === "number") {
+    isValid = isValid && objToValidate.value <= objToValidate.max;
+  }
+
+  return isValid;
+}
+// end of validation
 
 class ProjectInput {
   private templateEl: HTMLTemplateElement;
@@ -25,34 +66,87 @@ class ProjectInput {
   private inputPeopleEl: HTMLInputElement;
 
   constructor() {
-    this.templateEl = document.getElementById('project-input') as HTMLTemplateElement;
-    this.hostEl = document.getElementById('app') as HTMLDivElement;
+    this.templateEl = document.getElementById(
+      "project-input"
+    ) as HTMLTemplateElement;
+
+    this.hostEl = document.getElementById("app") as HTMLDivElement;
 
     const importedNode = document.importNode(this.templateEl.content, true);
 
     this.formEl = importedNode.firstElementChild as HTMLFormElement;
-    this.formEl.id = 'user-input';
+    this.formEl.id = "user-input";
 
-    this.inputTitleEl = document.querySelector('#title') as HTMLInputElement;
-    this.inputDescriptionEl = document.querySelector('#description') as HTMLTextAreaElement;
-    this.inputPeopleEl = document.querySelector('#people') as HTMLInputElement;
+    this.inputTitleEl = this.formEl.querySelector(
+      "#title"
+    )! as HTMLInputElement;
 
-    this.configure();
+    this.inputDescriptionEl = this.formEl.querySelector(
+      "#description"
+    ) as HTMLTextAreaElement;
+    this.inputPeopleEl = this.formEl.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
     this.attach();
+    this.configure();
+  }
+
+  private clearInputs() {
+    this.inputTitleEl.value = "";
+    this.inputDescriptionEl.value = "";
+    this.inputPeopleEl.value = "";
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const title = this.inputTitleEl.value.trim();
+    const description = this.inputDescriptionEl.value.trim();
+    const peopleAmount = +this.inputPeopleEl.value.trim();
+
+    const titleValidationConfig: Validatable = {
+      value: title,
+      required: true,
+    };
+
+    const descriptionValidationConfig: Validatable = {
+      value: description,
+      required: true,
+      minLength: 5,
+    };
+
+    const peopleValidationConfig: Validatable = {
+      value: peopleAmount,
+      required: true,
+      min: 1,
+    };
+
+    if (
+      !validate(titleValidationConfig) ||
+      !validate(descriptionValidationConfig) ||
+      !validate(peopleValidationConfig)
+    ) {
+      alert(
+        "An invalit input filed. Please, make sure all the fields are not empty"
+      );
+      return;
+    }
+
+    return [title, description, +peopleAmount];
   }
 
   @autobind
   private handleSubmit(event: Event) {
     event.preventDefault();
 
-    console.log('ADD PROJECT clicked', this.formEl);
-    console.log('titleEl', this.inputTitleEl);
-    console.log('descriptionEl', this.inputDescriptionEl);
-    console.log('peopleEl', this.inputPeopleEl);
+    const userInput = this.gatherUserInput();
+
+    console.log("userInput", userInput);
+
+    this.clearInputs();
   }
 
   private configure() {
-    this.formEl.addEventListener('submit', this.handleSubmit);
+    this.formEl.addEventListener("submit", this.handleSubmit);
   }
 
   private attach() {
